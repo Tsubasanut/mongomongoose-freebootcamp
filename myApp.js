5891afb86704d6b09e75ebb2753f81d6a9b7c312
tsubasanut <tsubasanut@gmail.com>
2022-05-12 15:18:09 +0300

    1st

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
+let mongodb = require("mongodb");
+let mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  //useUnifiedTopology: true,
+});
 
 let Person;
 
