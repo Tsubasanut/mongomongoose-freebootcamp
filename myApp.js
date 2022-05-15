4f951ca00584f50bbcfd0a86e2ba586273cecf94
tsubasanut <tsubasanut@gmail.com>
2022-05-15 23:40:35 +0300

    removeMany

--- myApp.js
+++ myApp.js
@@ -107,8 +107,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, data) => {
+    done(err, data);
+  });
+  //done(null /*, data*/);
 };
 
 const queryChain = (done) => {
